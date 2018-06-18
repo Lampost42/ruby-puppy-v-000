41f61c187ae2ec659d5f7f3c46e96699bdd7c2b1
Trevor Russell <truss.42@gmail.com>
2018-06-18 22:29:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
   
   @@all = []
-  
+  attr_reader :name
   def initialize(name)
     @name = name
     @@all << self
   end
   
   def self.all
-    @@all.each{|dog| puts @name}
+    @@all.each{|dog| puts dog.name}
   end
   
   def self.clear_all
